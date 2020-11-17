0f81b6ae387585df5e3f0d99fbaf0ea3d6e7ae35
andrea2702 <74329102+andrea2702@users.noreply.github.com>
2020-11-16 22:23:18 -0600

    Update evidencia_1.m

--- evidencia_1.m
+++ evidencia_1.m
@@ -1,10 +1,18 @@
-for i = 20:280
-    for n = 140:280
-        for m = 50:200
-            for j = 50:200
+clc
+clearvars
+
+contador=0;
+contador2=0;
+contador3=0;
+for i = 86:120
+    for n = 220:240
+        for m = 170:0.5:190
+            for j = 60:0.5:120
+                contador=contador + 1;
+                disp(contador)
                 quit=0;
                 x1 = 10;
-                x2 = i;
+                x2 =i;
                 x3 = n;
                 x4 = 280;
 
@@ -14,36 +22,52 @@ for i = 20:280
                 y4 = 120;
 
                 %Matriz principal
-                A = [(x1^3),(x1^2),(x1),1; (x2^3),(x2^2),(x2),1; (x3^3),(x3^2),(x3),1; (x4^3),(x4^2),(x4),1;];
+                A = [(x1^3),(x1^2),(x1),1; (x2^3),(x2^2),(x2),1; (x3^3),(x3^2),(x3),1; (x4^3),(x4^2),(x4),1];
 
                 %Vector Y
                 Y = [y1; y2; y3; y4];
 
                 %matriz de soluciones
-                Z = [];
 
                 Z = inv(A) * Y;
 
 
-
-                funcion = @(X)(Z(1)*X.^3)+(Z(2)*X.^2) + (Z(3)*X)+(Z(4));
-                funcion_2=@(X) (3*Z(1)*X.^2)+(2*Z(2)*X) + (Z(3));
-                funcion_3=@(X) ((6*Z(1)*X)+(2*Z(2)));
-                funcion_4=@(X) sqrt((1+(((3*Z(1)*X.^2)+(2*Z(2)*X) + (Z(3))).^2)));
-                longitud=integral(funcion_4,x1,x4);
-                %longitud=400;
-                if(longitud<500 && longitud>300)
-                    %xmax= Calcular min
-                    %xmin= calcular max
-                    %radio=((1+((funcion_2(xmax))^2))^(3/2))/(funcion_3(xmax));
-                    %radio_2=((1+(funcion_2(xmin)^2))^(3/2))/(funcion_3(xmin));
-                    if radio < 50 && radio_2 <50 && radio > 0 && radio_2 > 0
-                       disp(longitud)
-                       disp(radio)
-                       disp(radio_2)
-                       disp(Z)
-                       quit=1;
-                        break;
+                if (Z(1)~=0)
+                    funcion = @(X)(Z(1)*X.^3)+(Z(2)*X.^2) + (Z(3)*X)+(Z(4));
+                    funcion_2=@(X) (3*Z(1)*X.^2)+(2*Z(2)*X) + (Z(3));
+                    funcion_3=@(X) ((6*Z(1)*X)+(2*Z(2)));
+                    funcion_4=@(X) sqrt((1+(((3*Z(1)*X.^2)+(2*Z(2)*X) + (Z(3))).^2)));
+                    longitud=integral(funcion_4,x1,x4);
+                    %longitud=400;
+                    if(longitud<500 && longitud>300)
+                        cont=1;
+                        index=1;
+                        X=[x1:x4];
+                        Y=[];
+                        for k=x1:x4
+                            Y(cont)=funcion(k);
+                            dy = funcion_2(k);
+                            if dy==0
+                                xmin=k;
+                            end
+                            cont=cont+1;
+                        end
+                        contador2=contador2+1;
+                        try
+                            radio=((1+(funcion_2(xmin)^2))^(3/2))/(funcion_3(xmin));
+                        catch
+                            radio=51;
+                        end
+                        if radio < 50 && radio>-50
+                            disp(longitud)
+                            disp(radio)
+                            disp(Z)
+                            quit=1;
+                            plot(X,Y)
+                            disp(X)
+                            disp(Y)
+                           break;
+                        end
                     end
                 end
             end
@@ -58,4 +82,7 @@ for i = 20:280
     if quit == 1
        break;
    end
-end 
+end
+
+disp(contador2)
+disp(contador)
